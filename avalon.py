1263f89ab9b78f996f3ff67e0305055c0d7898cd
xiangpeng <zhangxiangpeng2008@gmail.com>
2019-08-18 15:58:19 +0800

    try google auth

--- avalon.py
+++ avalon.py
@@ -1,12 +1,28 @@
-import dash
+from dash import Dash
 import json
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
+# from flask import request,Flask
 from flask import request
+# from dash_google_auth import GoogleOAuth
+
+# server = Flask(__name__)
+# server.config.update({
+    # 'GOOGLE_OAUTH_CLIENT_ID': '393849065511-j9mq7bms2e8a13r94onvjdhel6vl8go3.apps.googleusercontent.com',
+    # 'GOOGLE_OAUTH_CLIENT_SECRET': 'pUEBVllmHoHGstviLMVGCADs',
+    # })
+
+
+# authorized_emails = []
+# additional_scopes = []
 
 rowNb=12
-app = dash.Dash(__name__)
+fontSize=20
+# app = Dash(__name__, server=server, url_base_pathname='/', meta_tags=[ {"name": "viewport", "content": "width=device-width, initial-scale=1"} ])
+app = Dash(__name__, meta_tags=[ {"name": "viewport", "content": "width=device-width, initial-scale=1"} ])
+# app.secret_key = 'super23'
+# auth = GoogleOAuth(app, authorized_emails, additional_scopes)
 app.layout = html.Div([
     html.Div(id='cache_history', style={'display': 'none'}),
     dcc.Textarea(
@@ -14,10 +30,12 @@ app.layout = html.Div([
         readOnly=True,
         className='textarea',
         rows=rowNb,
+        style={'color': 'blue', 'fontSize': fontSize-3},
         ),
     html.Div(),
-    dcc.Input(id='input_cmd', type='text', placeholder='Enter your command here.',value='', debounce=True, className='input'),
-    html.Button(id='submit_button', n_clicks=0, children='Submit', className='button'),
+    dcc.Input(id='input_cmd', type='text', placeholder='Enter your command here.',value='', debounce=True, className='input', style={'fontSize': fontSize},),
+        
+    html.Button(id='submit_button', n_clicks=0, children='Submit', className='button', style={'fontSize': fontSize}),
     ])
 
 @app.callback(
@@ -35,7 +53,7 @@ def update_output_div(click, input_value, existe_value):
         res=['Interaction history']
     else:
         res=json.loads(existe_value)[1-rowNb:]
-    return [json.dumps(res+[input_value]),'x']
+    return [json.dumps(res+[input_value]),'']
 
 
 @app.callback(
@@ -48,4 +66,5 @@ def update_output_div(existe_value):
     return 'Interaction history'
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    # app.run_server(debug=True, port = 7777)
+    app.run_server(debug=True,host='0.0.0.0',port = 7777)
--- server.q
+++ server.q
@@ -1,4 +1,6 @@
+players:(5+til 6)! (`MERLIN`PERCIVAL`SERVANT`MORGANA`ASSASSIN; `MERLIN`PERCIVAL`SERVANT`SERVANT`MORGANA`ASSASSIN; `MERLIN`PERCIVAL`SERVANT`SERVANT`MORGANA`ASSASSIN`OBERON; `MERLIN`PERCIVAL`SERVANT`SERVANT`SERVANT`MORGANA`ASSASSIN`MINION; `MERLIN`PERCIVAL`SERVANT`SERVANT`SERVANT`SERVANT`MORDRED`ASSASSIN`MORGANA; `MERLIN`PERCIVAL`SERVANT`SERVANT`SERVANT`SERVANT`MORDRED`ASSASSIN`MORGANA`OBERON)
+rols:`MERLIN`PERCIVAL`SERVANT`ASSASSIN`MORGANA`OBERON`MORDRED`MINION!("Knows evil, must remain hidden"; "Knows Merlin"; "Loyal Servant of Arthur"; "Minion of Mordred"; "Appears as Merlin"; "Unkonwn to Evil"; "Unknow to Merlin"; "Minion of Mordred");
 
-creat:{[nb]
-    if[not nb within 
-    }
+creat:{[nb] $[nb in key players; [`rols_round set (neg nb)?players[nb];:raze "`",/:string players[nb];];:"please enter of number between ",(string min key players)," and ",(string max key players)] };
+shuffle:{ `rols_round set (neg count rols_round)?rols_round};
+
