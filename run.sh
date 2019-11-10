6fb4868661826f5f7e664f4c52b408dc6e35e4f2
xiangpeng <zhangxiangpeng2008@gmail.com>
2019-11-10 11:57:19 +0800

    modify run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
+cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
+
+mkdir -p log
+currentTime="`date +%Y%m%d_%H%M%S`"
+kill $(ps aux | grep 'server.cpp.o 20001' | awk '{print $2}')
+kill $(ps aux | grep '7777 avalon' | awk '{print $2}')
+nohup ./server.cpp.o 20001 &> log/cpp_$currentTime.txt &
+sleep 3
 source ~/venv37/bin/activate
-gunicorn --bind 0.0.0.0:7777 avalon:server
+nohup gunicorn --bind 0.0.0.0:7777 avalon:server &> log/dash_$currentTime.txt &
+gunicorn --bind 0.0.0.0:7777 avalon:server 
